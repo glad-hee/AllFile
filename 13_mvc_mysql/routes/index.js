8f544fa562f9e81cd7ef3e8977bb1dec1bacf3f8
glad-hee <illesteva0@naver.com>
2023-04-06 18:48:58 +0900

    mysql delete

--- 13_mvc_mysql/routes/index.js
+++ 13_mvc_mysql/routes/index.js
@@ -1,11 +1,17 @@
-const express = require('express');
-const controller = require('../controller/Cvisitor');
+const express = require("express");
+const controller = require("../controller/Cvisitor");
 const router = express.Router();
 
 // GET / => localhost:PORT/
-router.get('/', controller.main);
+router.get("/", controller.main);
 
 // GET /visitor => localhost:PORT/visitor
-router.get('/visitor', controller.getVisitors); // 전체 조회
+router.get("/visitor", controller.getVisitors); // 전체 조회
+
+// POST /visitor/write
+router.post("/visitor/write", controller.postVisitor); // 하나 추가
+
+// DELETE /visitor/delete
+router.delete("/visitor/delete", controller.deleteVisitor); // 삭제
 
 module.exports = router;
